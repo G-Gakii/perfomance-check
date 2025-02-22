0bdf08902d20ebd6c9de59c6a036d1158483a3c8
“G-Gakii” <gakiiviolet1@gmail.com>
2025-02-22 22:32:45 +0300

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { environment } from '../environments/environment.development';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'checkperfomance';
+  name = environment.name;
 }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+     name: 'mike check',
+    
+};
